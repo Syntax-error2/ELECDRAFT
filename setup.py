59ee2659b1a90f438ce0df645fd1938a444e5644
Syntax-error2 <130565173+Syntax-error2@users.noreply.github.com>
2026-02-03 21:57:49 +0800

    commit all

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ELECDRAFT',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/Syntax-error2/ELECDRAFT',
+    license='Elecdraft Pro',
+    author='LENOVO',
+    author_email='Davehermoso01@gmail.com',
+    description='A project for Cavite State University CCAT Campus'
+)
